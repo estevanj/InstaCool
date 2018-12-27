5ce9819044bcb7c237c4393780205cc50b6e2ad7
Estevan <estevanjavier@hotmail.com>
2018-12-27 14:41:13 -0500

    Creacion file componentes

---
+++ src/components/headers.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import logo from '../logo.svg';
+
+export default class Header extends React.Component{
+    public render(){
+        return (
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+        )
+    }    
+} 
+
